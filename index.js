955b129c795fcaf1b82f4e22d8c776ea0466377d
Joe Glass <glass_joseph@hotmail.com>
2023-06-12 13:50:01 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,35 +1,103 @@
 // start by creating data so we don't have to type it in each time
 let dataArray = [];
-dataArray.push("first");
-dataArray.push("second");
-dataArray.push("third");
+
+// define a constructor to create  objects
+let ReviewObject = function (pName, pCity, pCuisine, pStars, pPoster) {
+    this.name = pName;
+    this.city = pCity;
+    this.cuisine = pCuisine;  // action  comedy  drama  horrow scifi  musical  western
+    this.stars = pStars;
+    this.poster = pPoster;
+}
 
 
 document.addEventListener("DOMContentLoaded", function () {
 
     document.getElementById("getServerData").addEventListener("click", function () {
-        createList();  
+        createList("all", "");  
     });
+
+    document.getElementById("getCity").addEventListener("click", function () {
+        let which = document.getElementById("whichCity").value;
+        createList("city",which);  
+    });
+
+    document.getElementById("postreview").addEventListener("click", function () {
+        postnew();  
+    });
+    
+    function StartWebListener()   
+    {
+        $.get("https://newlistenwritemongo.azurewebsites.net/", function(data, status){  });
+    }
+    
 });
 
+function postnew() {
+    let newName = document.getElementById("name").value;
+    let newCity = document.getElementById("city").value;
+    let newCuisine = document.getElementById("cuisine").value;
+    let newStars = parseInt(document.getElementById("stars").value);
+    let newPoster = document.getElementById("poster").value;
+
+    let newOne = new ReviewObject(newName, newCity, newCuisine, newStars, newPoster);
+    console.log(newOne);
+
+   
+    $.ajax({
+    	// run locally
+        //url : "http://localhost:7071/api/kurtrestaurantwriter",
+        
+        
+        
+        // run on azure pre pubsub
+        url : "https://joewritemongo.azurewebsites.net/api/joerestaurantwriter",
+        
+         // run on azure with  pubsub
+        
+        //url : "https://joecosmosdatawriter.azurewebsites.net/api/joewritetocosmosinclass",
+
+        
+        
+        type: "POST",
+        data: JSON.stringify(newOne),
+        contentType: "application/json; charset=utf-8",  
+        success: function (result) {
+            console.log(result);
+            document.getElementById("name").value = "";
+            document.getElementById("city").value = "";
+            document.getElementById("cuisine").value = ""
+            document.getElementById("stars").value = "";
+            document.getElementById("poster").value = "";
+            
+          }
+        });
+}
   
-function createList() {
-    
-	// run locally
-    //$.get("http://localhost:7071/api/kurtRestaurant", function(data, status){ 
-    
-    
-	// run on Azure
-    $.get("https://joecosmosdatawriter.azurewebsites.net/api/joewritetocosmosinclass", function(data, status){ 
-        dataArray = JSON.parse(data);
-        console.log(dataArray);
+function createList(which, city) {
+    let param = "all";
+    if(which == "city"){
+        param = city;
+    }
+
+    // run in cloud
+    $.get("https://kurtreadfrommongo.azurewebsites.net/api/readfrommongo/?name="+ param, function(data, status){
+ 
+     // run local
+      //$.get("http://localhost:7071/api/readfrommongo/?name=" + param, function(data, status){ 
+
+    dataArray = JSON.parse(data);
+    subsetArray = [];
+    for(i = 0; i < dataArray.length; i++){
+         subsetArray[i] = dataArray[i];
+     }
        
-                
+                      
       //clear prior data
       let table = document.getElementById("tableResult");        
       table.innerHTML = "";  // remove previous data
 
-       var header = table.createTHead();
+        var header = table.createTHead();
         var hRow = header.insertRow(0);
         var hCell1 = hRow.insertCell(0);
         var hCell2 = hRow.insertCell(1);
@@ -43,25 +111,19 @@ function createList() {
         hCell5.innerHTML = "<b>Poster</b>";
        
 
-        for (let i = 0; i < dataArray.length; i++) {
+        for (let i = 0; i < subsetArray.length; i++) {
             let row = table.insertRow(-1);
             let cell1 = row.insertCell(0);
             let cell2 = row.insertCell(1);
             let cell3 = row.insertCell(2);
             let cell4 = row.insertCell(3);
             let cell5 = row.insertCell(4);
-            cell1.innerHTML = dataArray[i].name;
-            cell2.innerHTML = dataArray[i].city;
-            cell3.innerHTML = dataArray[i].cuisine 
-            cell4.innerHTML = dataArray[i].stars;
-            cell5.innerHTML = dataArray[i].poster;
+            cell1.innerHTML = subsetArray[i].name;
+            cell2.innerHTML = subsetArray[i].city;
+            cell3.innerHTML = subsetArray[i].cuisine 
+            cell4.innerHTML = subsetArray[i].stars;
+            cell5.innerHTML = subsetArray[i].poster;
         }
 
     });
 }
-  
-
-
- 
-
-
